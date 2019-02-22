f3486b18051276ba199f0f1d8779252c1b7a88b5
Maximo Bautista <max.bautista97@gmail.com>
2019-02-22 01:17:25 -0500

    added slug and song class

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,4 +8,10 @@ class ArtistsController < ApplicationController
     erb :"artists/index"
   end
 
+  get '/show/:slug' do
+    @artist = Artist.find_by_slug(params[:slug])
+    erb :"artists/show"
+  end
+
+
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,15 @@
 class Artist < ActiveRecord::Base
     has_many :songs
     has_many :genres, through: :songs
+
+    def slug
+      self.name.downcase.gsub(/[^A-Za-z]+/, '-')
+    end
+
+    def self.find_by_slug(artist_slug)
+      self.all.select do |artist|
+        artist.slug == artist_slug
+      end.first
+    end
+
 end
